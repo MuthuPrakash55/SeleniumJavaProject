7e7cdce8d2a3686d3902e0d92d2ddccabb6a4a4d
MuthuPrakash <prakashmuthu17@gmail.com>
2023-03-08 10:19:46 +0530

    day01

---
+++ src/day01/program02.java
@@ -0,0 +1,23 @@
+package day01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+        driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("vijay");
+        driver.findElement(By.xpath("//textarea[@rows='3']")).sendKeys("chennai");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("12342@gmail.com");
+        driver.findElement(By.xpath("//input[@value='Male']")).click();
+        driver.findElement(By.xpath("//input[@value='Cricket']")).click();
+        driver.findElement(By.xpath("//input[@ng-model='FirstName']")).clear();
+        //driver.findElement(By.xpath("//button[@id='Button1']")).click();
+	    driver.findElement(By.xpath("//select[@id='Skills']")).click();
+	}
+
+}
